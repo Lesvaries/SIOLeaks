758879ff6ed10f32ad038a23304df5808ebae9ea
Mathis Dupont <mathis.dupont@stcharles-stecroix>
2025-10-03 15:23:58 +0200

    favicon_setup

---
+++ app/views/layouts/header.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIOLeaks</title>
+
+    <!-- Favicon links -->
+    <link rel="icon" type="image/x-icon" href="./public/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="./public/assets/img/favicon/favicon-32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./public/assets/img/favicon/favicon-16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./public/assets/img/favicon/apple-touch-icon.png">
+    <link rel="manifest" href="./public/site.webmanifest">
+    <link rel="icon" type="image/png" sizes="192x192" href="./public/assets/img/favicon/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="./public/assets/img/favicon/android-chrome-512x512.png">
+
+    <!-- Stylesheet link -->
+    <link rel="stylesheet" href="/assets/styles.css">
+</head>
--- index.php
+++ index.php
@@ -1,1 +1,4 @@
+<?php
+    include_once './app/views/layouts/header.php';
+?>
 <p>test</p>
