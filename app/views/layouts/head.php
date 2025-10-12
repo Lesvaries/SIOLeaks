fccc149cadaae51a6a8e2ae494021accfda74d45
Mathis Dupont <mathis.dupont@stcharles-stecroix>
2025-10-12 16:40:30 +0200

    Dark Theme Done

--- app/views/layouts/head.php
+++ app/views/layouts/head.php
@@ -22,4 +22,12 @@
     <!-- Stylesheet link -->
     <link rel="stylesheet" href="./public/assets/css/main.css">
 
+    <script>
+    // Vérifie le thème dans localStorage et applique immédiatement
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.documentElement.classList.add('dark-theme');
+    }
+    </script>
+
 </head>
