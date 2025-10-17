dbd8b4dac9fa037aba7a9dbf04a5a7302b32bc2b
Mathis Dupont <mathis.dupont@stcharles-stecroix>
2025-10-17 13:33:29 +0200

    footer_v1.01

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,9 +1,23 @@
         
-        <footer>
-            <section>
-                <div class="container text-center py-3">
-                    <p>&copy; 2024 SIOLeaks. Tous droits réservés.</p>
+        <footer style="background: red;">
+            <section class="container-fluid">
+                <div class="row top-footer">
+                    <div class="col-4 top-footer-content">
+                        <p>Contact</p>
+                    </div>
+                    <div class="col-4 top-footer-content">
+                        <p>About us</p>
+                    </div>
+                    <div class="col-4 top-footer-content">
+                        <p>About us</p>
+                    </div>
                 </div>
+                <div class="row">
+                    <div class="container text-center py-3">
+                        <p>&copy; 2024 SIOLeaks. Tous droits réservés.</p>
+                    </div>
+                </div>
+
             </section>
         </footer>
         
