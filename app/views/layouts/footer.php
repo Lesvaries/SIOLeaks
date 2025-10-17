ea6fd573e1e28298496985718c59e1fc9acf8096
Mathis Dupont <mathis.dupont@stcharles-stecroix>
2025-10-17 23:08:56 +0200

    footer_v1.03

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,29 +1,46 @@
             
-            <footer style="background: red;">
+            <footer>
                 <section class="container-fluid">
                     <div class="row top-footer">
                         <div class="col-4 top-footer-content">
-                            <p><strong>Contact</strong></p>
-                            <div class="list.contact">
+                            <h2><strong>Contact</strong></h2>
                                 <ol>
                                     <li>SIOLeaks.team@gmail.com</li>
                                 </ol>
-                            </div>
-
                         </div>
                         <div class="col-4 top-footer-content">
-                            <p><strong>About us</strong></p>
+                            <h2><strong>Mission</strong></h2>
+                            <p class="small">
+                                Partageons le savoir, gratuitement.
+                            </p>
                         </div>
                         <div class="col-4 top-footer-content">
-                            <p><strong>Contact</strong></p>
+                            <h2><strong>Contact</strong></h2>
                         </div>
                     </div>
                     <div class="row bottom-footer">
-                        <div class="row">
-                            <div class="col-3">
-                                <img src="../../../public/assets/img/icon/logo.png" alt="icon" class="logo-footer">
+                        <div class="row footer-content">
+                            <div class="col-3 logo-footer">
+                                <img src="../../../public/assets/img/icon/logo.png" alt="icon">
                             </div>                        
-
+                            <div class="col-3">
+                                <ul>
+                                    <h2><strong><span>¬</span>Navigation</strong></h2>
+                                    <li><a href="index.php?section=index">Accueil</a></li>
+                                    <li><a href="index.php?section=cours">Cours</a></li>
+                                    <li><a href="index.php?section=about">A propos</a></li>
+                                    <li><a href="index.php?section=contact">Contact</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-3">
+                                <ul>
+                                    <h2><strong><span>¬</span>Liens Externes</strong></h2>
+                                    <li><a href="index.php?section=index">Mentions Légales</a></li>
+                                    <li><a href="index.php?section=cours">Politique de confidentialité</a></li>
+                                    <li><a href="index.php?section=about"></a></li>
+                                    <li><a href="index.php?section=contact">Contact</a></li>
+                                </ul>
+                            </div>
                         </div>
                         <div class="row">
                             <div class="container text-center py-3">
