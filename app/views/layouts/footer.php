59f3634c321ca9d985a1fa413eef6b7d13041f06
Mathis Dupont <mathis.dupont@stcharles-stecroix>
2025-10-18 13:37:06 +0200

    footer_v1.1

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,51 +1,55 @@
             
             <footer>
                 <section class="container-fluid">
+
                     <div class="row top-footer">
                         <div class="col-4 top-footer-content">
                             <h2><strong>Contact</strong></h2>
-                                <ol>
+                                <ul>
                                     <li>SIOLeaks.team@gmail.com</li>
-                                </ol>
+                                </ul>
                         </div>
                         <div class="col-4 top-footer-content">
                             <h2><strong>Mission</strong></h2>
-                            <p class="small">
+                            <p>
                                 Partageons le savoir, gratuitement.
                             </p>
                         </div>
                         <div class="col-4 top-footer-content">
-                            <h2><strong>Contact</strong></h2>
+                            <h2><strong>Placeholder</strong></h2>
+                            <p>
+                                à remplir plus tard...
+                            </p>
                         </div>
                     </div>
-                    <div class="row bottom-footer">
-                        <div class="row footer-content">
-                            <div class="col-3 logo-footer">
-                                <img src="../../../public/assets/img/icon/logo.png" alt="icon">
-                            </div>                        
-                            <div class="col-3">
-                                <ul>
-                                    <h2><strong><span>¬</span>Navigation</strong></h2>
-                                    <li><a href="index.php?section=index">Accueil</a></li>
-                                    <li><a href="index.php?section=cours">Cours</a></li>
-                                    <li><a href="index.php?section=about">A propos</a></li>
-                                    <li><a href="index.php?section=contact">Contact</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-3">
-                                <ul>
-                                    <h2><strong><span>¬</span>Liens Externes</strong></h2>
-                                    <li><a href="index.php?section=index">Mentions Légales</a></li>
-                                    <li><a href="index.php?section=cours">Politique de confidentialité</a></li>
-                                    <li><a href="index.php?section=about"></a></li>
-                                    <li><a href="index.php?section=contact">Contact</a></li>
-                                </ul>
-                            </div>
+
+                    <div class="row footer align-items-start">
+                        <div class="col-3 logo-footer">
+                            <img src="../../../public/assets/img/icon/logo.png" loading="lazy" alt="icon" class="img-fluid">
+                        </div>                        
+                        <div class="col-3 footer-content">
+                            <h2><strong><span>¬</span> Navigation</strong></h2>
+                            <ul>           
+                                <li><a href="index.php?section=index">Accueil</a></li>
+                                <li><a href="index.php?section=cours">Cours</a></li>
+                                <li><a href="index.php?section=about">A propos</a></li>
+                                <li><a href="index.php?section=contact">Contact</a></li>
+                            </ul>
                         </div>
-                        <div class="row">
-                            <div class="container text-center py-3">
-                                <p>&copy; 2024 SIOLeaks. Tous droits réservés.</p>
-                            </div>
+                        <div class="col-3 footer-content">
+                            <h2><strong><span>¬</span> Liens Externes</strong></h2>
+                            <ul>                                   
+                                <li><a href="index.php?section=index">Mentions Légales</a></li>
+                                <li><a href="index.php?section=cours">Politique de confidentialité</a></li>
+                                <li><a href="index.php?section=contact">Contact</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-3"></div>
+                    </div>
+
+                    <div class="row bottom-footer">
+                        <div class="container text-center py-3">
+                            <p>&copy; 2024 SIOLeaks. Tous droits réservés.</p>
                         </div>
                     </div>
 
