df24c77f2022fd14b9ddf34514181498da277eea
Mathis Dupont <mathis.dupont@stcharles-stecroix>
2025-10-18 14:55:41 +0200

    update header

--- public/assets/js/theme.js
+++ public/assets/js/theme.js
@@ -1,3 +1,19 @@
+// Fonction pour charger le thème sauvegardé
+function loadTheme() {
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme === 'dark') {
+        document.documentElement.classList.add('dark-theme');
+        document.getElementById('theme-icon').className = 'fas fa-moon';
+    } else {
+        document.documentElement.classList.remove('dark-theme');
+        document.getElementById('theme-icon').className = 'fas fa-sun';
+    }
+}
+
+// Appelle la fonction au chargement de la page
+document.addEventListener('DOMContentLoaded', loadTheme);
+
 // Ta fonction toggle mise à jour
 function toggle() {
     document.documentElement.classList.toggle('dark-theme');
