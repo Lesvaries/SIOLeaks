46cfb0529a8d9d8f467f6a44c1b3e0f14f4153f7
Mathis Dupont <mathis.dupont@stcharles-stecroix>
2025-11-16 21:34:18 +0100

    card program

--- app/views/programs.view.php
+++ app/views/programs.view.php
@@ -11,13 +11,211 @@
     </div>
 </section>
     
-    
-    
-    
-    
-    
-    
-    <p>Hello World (programs.php)</p>
+<section class="cours">
+    <article data-year="year1">
+        <h1>Année 1</h1>
+
+        <div class="matiere" data-matiere="dev">
+            <h2>Développement</h2>
+            <div class="card-container">
+                <div class="card-cours" data-chapitre="chap1">
+                    <h3>Chapitre 1</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap2">
+                    <h3>Chapitre 2</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap3">
+                    <h3>Chapitre 3</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="matiere" data-matiere="bdd">
+            <h2>Base de Données</h2>
+            <div class="card-container">
+                <div class="card-cours" data-chapitre="chap1">
+                    <h3>Chapitre 1</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap2">
+                    <h3>Chapitre 2</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap3">
+                    <h3>Chapitre 3</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="matiere" data-matiere="sys" data-specialisation="sisr">
+            <h2>Système</h2>
+            <div class="card-container">
+                <div class="card-cours" data-chapitre="chap1">
+                    <h3>Chapitre 1</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap2">
+                    <h3>Chapitre 2</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap3">
+                    <h3>Chapitre 3</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="matiere" data-matiere="reseau" data-specialisation="sisr">
+            <h2>Réseaux</h2>
+            <div class="card-container">
+                <div class="card-cours" data-chapitre="chap1">
+                    <h3>Chapitre 1</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap2">
+                    <h3>Chapitre 2</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap3">
+                    <h3>Chapitre 3</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="matiere" data-matiere="algo">
+            <h2>Algorithmie</h2>
+            <div class="card-container">
+                <div class="card-cours" data-chapitre="chap1">
+                    <h3>Chapitre 1</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap2">
+                    <h3>Chapitre 2</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap3">
+                    <h3>Chapitre 3</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="matiere" data-matiere="cejm">
+            <h2>Culture économique, juridique et managériale</h2>
+            <div class="card-container">
+                <div class="card-cours" data-chapitre="chap1">
+                    <h3>Chapitre 1</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap2">
+                    <h3>Chapitre 2</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap3">
+                    <h3>Chapitre 3</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="matiere" data-matiere="maths">
+            <h2>Mathématique</h2>
+            <div class="card-container">
+                <div class="card-cours" data-chapitre="chap1">
+                    <h3>Chapitre 1</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap2">
+                    <h3>Chapitre 2</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap3">
+                    <h3>Chapitre 3</h3>
+                </div>
+            </div>
+        </div>
+    </article>
+
+    <!-- Année 2 -->
+    <article data-year="year2">
+        <h1>Année 2</h1>
+
+        <div class="matiere" data-matiere="dev">
+            <h2>Développement</h2>
+            <div class="card-container">
+                <div class="card-cours" data-chapitre="chap1">
+                    <h3>Chapitre 1</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap2">
+                    <h3>Chapitre 2</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap3">
+                    <h3>Chapitre 3</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="matiere" data-matiere="cybersec">
+            <h2>Cybersécurité</h2>
+            <div class="card-container">
+                <div class="card-cours" data-chapitre="chap1">
+                    <h3>Chapitre 1</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap2">
+                    <h3>Chapitre 2</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap3">
+                    <h3>Chapitre 3</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="matiere" data-matiere="sys" data-specialisation="sisr">
+            <h2>Système</h2>
+            <div class="card-container">
+                <div class="card-cours" data-chapitre="chap1">
+                    <h3>Chapitre 1</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap2">
+                    <h3>Chapitre 2</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap3">
+                    <h3>Chapitre 3</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="matiere" data-matiere="reseau" data-specialisation="sisr">
+            <h2>Réseaux</h2>
+            <div class="card-container">
+                <div class="card-cours" data-chapitre="chap1">
+                    <h3>Chapitre 1</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap2">
+                    <h3>Chapitre 2</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap3">
+                    <h3>Chapitre 3</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="matiere" data-matiere="cejm">
+            <h2>Culture économique, juridique et managériale</h2>
+            <div class="card-container">
+                <div class="card-cours" data-chapitre="chap1">
+                    <h3>Chapitre 1</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap2">
+                    <h3>Chapitre 2</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap3">
+                    <h3>Chapitre 3</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="matiere" data-matiere="maths">
+            <h2>Mathématique</h2>
+            <div class="card-container">
+                <div class="card-cours" data-chapitre="chap1">
+                    <h3>Chapitre 1</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap2">
+                    <h3>Chapitre 2</h3>
+                </div>
+                <div class="card-cours" data-chapitre="chap3">
+                    <h3>Chapitre 3</h3>
+                </div>
+            </div>
+        </div>
+    </article>
+</section>
 
     
     
