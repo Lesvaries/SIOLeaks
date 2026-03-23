0f4957e734e0b315168740ba4ca2357c9a961c6f
Lesvaries <mathisdupont27@gmail.com>
2026-03-23 23:13:27 +0100

    fix: function courseModel

--- app/models/courseModels.php
+++ app/models/courseModels.php
@@ -5,10 +5,10 @@ function get_all_course_by_year($tag_year)
 
     $req = $bdd->prepare("SELECT * FROM subject WHERE tag_year = :tag_year");
     $req->execute([
-            tag_year = $tag_year;
-    
-        ]);
-    $allCourse = $req->fetchall(PDO::FETCH_ASSOC);
+        'tag_year' => $tag_year
+    ]);
+
+    return $req->fetchAll(PDO::FETCH_ASSOC);
 }
 
 
