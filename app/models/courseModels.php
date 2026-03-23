74ab08204c451f8309fd7bee3f01799b2072dc8c
zoelyne <zoelyne19@gmail.com>
2026-03-23 23:05:16 +0100

    WIP: share for all

--- app/models/courseModels.php
+++ app/models/courseModels.php
@@ -1,12 +1,14 @@
 <?php
-function get_all_by_year($tag_year)
+function get_all_course_by_year($tag_year)
 {
     global $bdd;
 
-    $req = $bdd->prepare("SELECT * FROM subject WHERE tag_year = 1");
+    $req = $bdd->prepare("SELECT * FROM subject WHERE tag_year = :tag_year");
     $req->execute([
-            
+            tag_year = $tag_year;
+    
         ]);
+    $allCourse = $req->fetchall(PDO::FETCH_ASSOC);
 }
 
 
