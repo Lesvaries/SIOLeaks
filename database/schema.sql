f733118c4152a6b9b3ed30107d9ca614ebde164c
Lesvaries <mathisdupont27@gmail.com>
2026-03-27 19:41:01 +0100

    FIX: Database (promis cette fois c'est la bonne)

--- database/schema.sql
+++ database/schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 27 mars 2026 à 19:15
+-- Généré le : ven. 27 mars 2026 à 19:40
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `sioleaks_bdd`
 --
+CREATE DATABASE IF NOT EXISTS `sioleaks_bdd` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `sioleaks_bdd`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `course`
+--
+
+CREATE TABLE `course` (
+  `id_course` int(11) NOT NULL,
+  `course_name` varchar(100) NOT NULL,
+  `subject` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `course_content`
+--
+
+CREATE TABLE `course_content` (
+  `id_course_content` int(11) NOT NULL,
+  `content` longtext NOT NULL,
+  `course` int(11) NOT NULL,
+  `upload` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -34,9 +61,61 @@ CREATE TABLE `subject` (
   `tag_year` enum('1','2') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `upload`
+--
+
+CREATE TABLE `upload` (
+  `id_upload` int(11) NOT NULL,
+  `path` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','superadmin','','') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `video_coursecontent`
+--
+
+CREATE TABLE `video_coursecontent` (
+  `id_video` int(11) NOT NULL,
+  `video_URL` varchar(100) NOT NULL,
+  `course_content` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id_course`),
+  ADD KEY `subject` (`subject`);
+
+--
+-- Index pour la table `course_content`
+--
+ALTER TABLE `course_content`
+  ADD PRIMARY KEY (`id_course_content`),
+  ADD KEY `upload` (`upload`),
+  ADD KEY `course` (`course`);
 
 --
 -- Index pour la table `subject`
@@ -45,14 +124,86 @@ ALTER TABLE `subject`
   ADD PRIMARY KEY (`id_subject`);
 
 --
+-- Index pour la table `upload`
+--
+ALTER TABLE `upload`
+  ADD PRIMARY KEY (`id_upload`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Index pour la table `video_coursecontent`
+--
+ALTER TABLE `video_coursecontent`
+  ADD PRIMARY KEY (`id_video`),
+  ADD KEY `course_content` (`course_content`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `course`
+--
+ALTER TABLE `course`
+  MODIFY `id_course` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `course_content`
+--
+ALTER TABLE `course_content`
+  MODIFY `id_course_content` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `subject`
 --
 ALTER TABLE `subject`
   MODIFY `id_subject` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `upload`
+--
+ALTER TABLE `upload`
+  MODIFY `id_upload` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `video_coursecontent`
+--
+ALTER TABLE `video_coursecontent`
+  MODIFY `id_video` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `course`
+--
+ALTER TABLE `course`
+  ADD CONSTRAINT `fk_course_id_subject` FOREIGN KEY (`subject`) REFERENCES `subject` (`id_subject`);
+
+--
+-- Contraintes pour la table `course_content`
+--
+ALTER TABLE `course_content`
+  ADD CONSTRAINT `course_content_ibfk_1` FOREIGN KEY (`upload`) REFERENCES `upload` (`id_upload`),
+  ADD CONSTRAINT `course_content_ibfk_2` FOREIGN KEY (`course`) REFERENCES `course` (`id_course`);
+
+--
+-- Contraintes pour la table `video_coursecontent`
+--
+ALTER TABLE `video_coursecontent`
+  ADD CONSTRAINT `video_coursecontent_ibfk_1` FOREIGN KEY (`course_content`) REFERENCES `course_content` (`id_course_content`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
