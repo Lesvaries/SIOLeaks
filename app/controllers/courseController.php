dff2624bf47d1f98a5f106464288eb816e7d7bbf
Lesvaries <mathisdupont27@gmail.com>
2026-03-27 15:40:11 +0100

    setup projet

--- app/controllers/courseController.php
+++ app/controllers/courseController.php
@@ -1,8 +1,6 @@
 <?php
 include_once '../models/courseModels.php';
 
-include_once '../views/course.php';
-
-
+include_once '../views/pages/course.php';
 
 ?>
---
+++ app/views/pages/homepage.php
@@ -0,0 +1,6 @@
+<?php
+    include_once '../app/views/partials/header.php';
+?>
+
+
+<a href="index.php?section=course">Cours</a>
