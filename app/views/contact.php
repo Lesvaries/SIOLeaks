674b2f5ab00a7cd32511218347f630158723ff3f
Victorien <ditomenev@gmail.com>
2026-03-27 17:06:20 +0100

    WIP: page contact

---
+++ app/views/contact.php
@@ -0,0 +1,21 @@
+<h1>Page de Cours</h1>
+<a href="index.php?section=subject">Contact</a>
+
+<div>
+    <div>
+        <p>Mathis DUPONT</p>
+        <p>Concepteur & Developpeur Principal </p>
+        <p>mathisdupont27@gmail.com</p>
+        <p></p>
+    </div>
+    <div>
+        <img src="https://i.pinimg.com/736x/97/b2/9f/97b29f76e155081f8cfad94c84bf4c7d.jpg"
+    </div>
+</div>
+<div>
+    
+<br>
+
+<div>
+
+</div>
